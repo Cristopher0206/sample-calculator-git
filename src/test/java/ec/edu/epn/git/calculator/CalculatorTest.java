787fcdb06fb5bc750bf0c7e63832e610f803cc93
Cristopher0206 <cristopher.perez@epn.edu.ec>
2022-06-12 20:09:08 -0500

    Commiting any other changes

--- src/test/java/ec/edu/epn/git/calculator/CalculatorTest.java
+++ src/test/java/ec/edu/epn/git/calculator/CalculatorTest.java
@@ -27,7 +27,7 @@ public class CalculatorTest {
     @Test
     public void given_two_integers_when_subtraction_then_ok(){
         System.out.println("Test 2");
-        assertEquals( 8, c.subtraction(10,4));
+        assertEquals( 6, c.subtraction(10,4));
     }
 
     @Test(expected = ArithmeticException.class)
